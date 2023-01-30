0d8334064d7e6e0929b8bc40f834d66b1ff26133
Khairil <101785282+KhairilOFC@users.noreply.github.com>
2023-01-30 20:30:08 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,11 +20,11 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : Zyko-MD
+${htjava} *💌 Nama* : Khairil春
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
-${htjava} *🎨 Umur* : 18 🥶
+${htjava} *🎨 Umur* : 14 🥶
 ${htjava} *🧮 Kelas* : private
 ${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP "MODDER WHATSAPP"
 ${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
